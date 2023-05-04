4cd2a8619f56fc8b5fdbf43bde140ec74ca10f8e
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2023-05-04 20:20:30 +0200

    Create TabSwitcher.tsx
    created TabSwitcher.tsx

---
+++ frontend/components/TabSwitcher.tsx
@@ -0,0 +1,4 @@
+import React, { useState } from "react";
+import { SelectButtons, useGlobalConfig } from "@airtable/blocks/ui";
+import { useUserProfile } from "../hooks/useUserProfile";
+import { TabSwitcherStyle } from '../TabSwitcherStyle'
